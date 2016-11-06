67ece1a2446f1fbf31f51313d801f9e290689b5b
Marius Norøy Lian <mal@dips.no>
2016-11-06 15:37:39 +0100

    ping controller

---
+++ FoodBackend/Food.Server.WebApi/Ping/PingController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Food.Server.WebApi.Ping
+{
+   public class PingController : ApiController
+    {
+        public  string Get()
+        {
+            return "Food Server is up";
+        }
+    }
+}
