e71e5522f2f7ce6176181c5fc668a51c243c9bfd
Marius Norøy Lian <mal@dips.no>
2016-11-08 21:00:27 +0100

    Database structure and data backup

--- Database/Dump20161108.sql
+++ Database/Dump20161108.sql
@@ -1,8 +1,10 @@
--- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
+CREATE DATABASE  IF NOT EXISTS `app` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `app`;
+-- MySQL dump 10.13  Distrib 5.6.24, for Win64 (x86_64)
 --
 -- Host: localhost    Database: app
 -- ------------------------------------------------------
--- Server version	5.7.16-log
+-- Server version	5.6.26-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -26,6 +28,9 @@ CREATE TABLE `dish` (
   `Id` int(11) NOT NULL,
   `Name` varchar(255) NOT NULL,
   `Description` varchar(255) DEFAULT NULL,
+  `Recipe` varchar(255) DEFAULT NULL,
+  `Difficulty` int(11) DEFAULT NULL,
+  `Duration` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -36,6 +41,7 @@ CREATE TABLE `dish` (
 
 LOCK TABLES `dish` WRITE;
 /*!40000 ALTER TABLE `dish` DISABLE KEYS */;
+INSERT INTO `dish` VALUES (1,'Rømmegrøt','grøtrett',NULL,NULL,NULL),(1791711019,'Pølse i brød','kjøttrett',NULL,NULL,NULL);
 /*!40000 ALTER TABLE `dish` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -51,11 +57,11 @@ CREATE TABLE `dish_ingredient` (
   `Amount` int(11) DEFAULT NULL,
   `Unit` varchar(255) DEFAULT NULL,
   `Ingredient_id_fk` int(11) NOT NULL,
-  `Disht_id_fk` int(11) NOT NULL,
+  `Dish_id_fk` int(11) NOT NULL,
   PRIMARY KEY (`Id`),
   KEY `Ingredient_id_fk_idx` (`Ingredient_id_fk`),
-  KEY `Disht_id_fk_idx` (`Disht_id_fk`),
-  CONSTRAINT `Disht_id_fk` FOREIGN KEY (`Disht_id_fk`) REFERENCES `dish` (`Id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  KEY `Disht_id_fk_idx` (`Dish_id_fk`),
+  CONSTRAINT `Disht_id_fk` FOREIGN KEY (`Dish_id_fk`) REFERENCES `dish` (`Id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `Ingredient_id_fk` FOREIGN KEY (`Ingredient_id_fk`) REFERENCES `ingredient` (`Id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -66,7 +72,37 @@ CREATE TABLE `dish_ingredient` (
 
 LOCK TABLES `dish_ingredient` WRITE;
 /*!40000 ALTER TABLE `dish_ingredient` DISABLE KEYS */;
+INSERT INTO `dish_ingredient` VALUES (1757351271,2,'Liter',2,1);
 /*!40000 ALTER TABLE `dish_ingredient` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `dish_tag`
+--
+
+DROP TABLE IF EXISTS `dish_tag`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `dish_tag` (
+  `Id` int(11) NOT NULL,
+  `Dish_id_fk` int(11) NOT NULL,
+  `Tag_id_fk` int(11) NOT NULL,
+  PRIMARY KEY (`Id`),
+  KEY `Dish_id_fk_idx` (`Dish_id_fk`),
+  KEY `TagId _fk_idx` (`Tag_id_fk`),
+  CONSTRAINT `DishInTag_id_fk` FOREIGN KEY (`Dish_id_fk`) REFERENCES `dish` (`Id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `TagId _fk` FOREIGN KEY (`Tag_id_fk`) REFERENCES `tag` (`Id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `dish_tag`
+--
+
+LOCK TABLES `dish_tag` WRITE;
+/*!40000 ALTER TABLE `dish_tag` DISABLE KEYS */;
+INSERT INTO `dish_tag` VALUES (1,1,899627983),(2,1791711019,928694354),(3,1791711019,843741084),(4,1791711019,297681079),(1333945632,1791711019,805764724);
+/*!40000 ALTER TABLE `dish_tag` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -90,36 +126,32 @@ CREATE TABLE `ingredient` (
 
 LOCK TABLES `ingredient` WRITE;
 /*!40000 ALTER TABLE `ingredient` DISABLE KEYS */;
+INSERT INTO `ingredient` VALUES (1,'Melk','Melkeprodukt'),(2,'Smør','Meieriprodukt'),(3,'Rømme','Melkeprodukt'),(4,'Fløte','Melkeprodukt'),(5,'Bacon','Kjøttprodukt');
 /*!40000 ALTER TABLE `ingredient` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `recipe`
+-- Table structure for table `tag`
 --
 
-DROP TABLE IF EXISTS `recipe`;
+DROP TABLE IF EXISTS `tag`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `recipe` (
-  `id` int(11) NOT NULL,
-  `Dish_id_fk` int(11) DEFAULT NULL,
-  `Description` varchar(255) DEFAULT NULL,
-  `Description_short` varchar(255) DEFAULT NULL,
-  `Difficulty` int(11) DEFAULT NULL,
-  `Duration` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `Dish_id_fk_idx` (`Dish_id_fk`),
-  CONSTRAINT `Dish_id_fk` FOREIGN KEY (`Dish_id_fk`) REFERENCES `dish` (`Id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+CREATE TABLE `tag` (
+  `Id` int(11) NOT NULL,
+  `Name` varchar(255) NOT NULL,
+  PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `recipe`
+-- Dumping data for table `tag`
 --
 
-LOCK TABLES `recipe` WRITE;
-/*!40000 ALTER TABLE `recipe` DISABLE KEYS */;
-/*!40000 ALTER TABLE `recipe` ENABLE KEYS */;
+LOCK TABLES `tag` WRITE;
+/*!40000 ALTER TABLE `tag` DISABLE KEYS */;
+INSERT INTO `tag` VALUES (151698370,'Mexico'),(297681079,'Fest'),(805764724,'Spicy'),(843741084,'Rask'),(899627983,'Tradisjonell'),(928694354,'Grille'),(1107510883,'Hverdag'),(1444907797,'Norsk'),(1921483138,'Kosemat');
+/*!40000 ALTER TABLE `tag` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -131,4 +163,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-11-06 15:50:29
+-- Dump completed on 2016-11-08 20:57:12
