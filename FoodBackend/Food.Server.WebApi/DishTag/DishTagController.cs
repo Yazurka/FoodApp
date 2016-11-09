349e12c05194a01ccc19fa32d938b11bdb516841
Marius Norøy Lian <mal@dips.no>
2016-11-09 17:32:40 +0100

    DishTagController post delete get

---
+++ FoodBackend/Food.Server.WebApi/DishTag/DishTagController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Food.Server.DishTag;
+using Food.Server.Tag;
+
+namespace Food.Server.WebApi.DishTag
+{
+    public class DishTagController : ApiController
+    {
+        private readonly IDishTagService m_dishTagService;
+
+        public DishTagController(IDishTagService dishTagService)
+        {
+            m_dishTagService = dishTagService;
+        }
+
+        public async Task Post(DishTagCreateRequest dishTagCreateRequest)
+        {
+            await m_dishTagService.AddTagsToDish(dishTagCreateRequest);
+        }
+        public async Task Delete(int dishId, int[] tagIds)
+        {
+            await m_dishTagService.RemoveTagsFromDish(dishId,tagIds);
+        }
+
+        public async Task<IEnumerable<TagResult>> Get(int dishId)
+        {
+            var result = await m_dishTagService.FindTagsForDish(dishId);
+            return result;
+        }
+    }
+}
