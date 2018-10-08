3b68d03064c7684daecabacf8bdfd8dbfa6889ca
Frederik Tegnander <frederik.t91@gmail.com>
2018-10-08 20:13:07 +0200

    Added routing to details page from index.

---
+++ FoodBackend/Food.Server.WebApi/Pages/Dish/Details.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Food.Server.Dish;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Food.Server.WebApi.Pages.Dish
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly IDishService _dishService;
+
+        public Server.Dish.Dish Dish { get; set; }
+
+        public DetailsModel(IDishService dishService)
+        {
+            _dishService = dishService;
+        }
+
+        public async Task OnGet(int id)
+        {
+            Dish = await _dishService.FindDish(id);
+        }
+    }
+}
